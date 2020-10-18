ceb0fb994b96f56a88eed4c89a60d7ccc95cedb1
tanvesh <sarvetanvesh01@gmail.com>
2020-10-18 22:41:43 +0530

    set up api of unsplash

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,94 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-
+import React, { useState } from "react";
+import {
+    IonApp,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    IonIcon,
+    IonList,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonCardSubtitle
+} from "@ionic/react";
+import { alertOutline } from "ionicons/icons";
+// @ts-ignore
+import Unsplash, { toJson } from "unsplash-js";
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+    let tasks: Array<String> = ["Tanvesh"];
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+    const [text, setText] = useState<number>();
+    const [url, setUrl] = useState<Array<any>>();
+    const unsplash = new Unsplash({
+        accessKey: "JIQRCSRMQCe0JISYi1KtI0uDJqg0MCAGnC6w5C78Co0",
+    });
+    let count;
+    const getImages = () =>{
+        // unsplash.photos.getRandomPhoto({ count: text }).then(toJson).then((json :any) =>{
+        //     console.log(json);
+        //     setUrl(json);
+        //     // console.log(json.urls.thumb)
+        // })
+        unsplash.collections.getCollectionPhotos(5020650, 1 , 10, "latest")
+            .then(toJson)
+            .then((json : any) => {
+                console.log(json)
+                setUrl(json)
+            });
+    }
+    // useEffect(() => {
+    // // let thumb, subtitle;
+        
+    // }, [])
+    return (
+        <IonApp>
+            <IonContent className="ion-padding">
+                <IonGrid>
+                    <IonRow className="ion-align-items-center">
+                        <IonCol className="ion-text-center">
+                            <IonButton onClick={()=> getImages() } color="primary">
+                                <IonIcon slot="start" icon={alertOutline} />
+                                
+                            </IonButton>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <h2>{text}</h2>
+                {url?.map(el=>{
+                    return <div>
+                        <img src={el.urls.thumb} alt="" style={{width:"100%"}} />
+                        {/* <IonCardHeader>
+                            <IonCardSubtitle></IonCardSubtitle>
+                        </IonCardHeader> */}
+                </div>
+                })}
+                
+            </IonContent>
+        </IonApp>
+    );
+};
 
 export default App;
